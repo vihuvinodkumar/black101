a64e4dc54f2c76882e7ae32a7d268606b58e4d86
vinod <vinod@pcodertechnologies.us>
2023-06-01 10:20:51 +0530

    panding..

--- app/Http/Controllers/StripePaymentController.php
+++ app/Http/Controllers/StripePaymentController.php
@@ -25,40 +25,52 @@ class StripePaymentController extends Controller
  */
 public function stripePost(Request $request)
 {
-    // print_r($request->all()); die();
     Stripe\Stripe::setApiKey('sk_test_51N7V4CD0tzja3zlTmYDbCgBgOZlh66z5OUxM27QrsP1YC5HlWH1OHsLQ6oSQ82QncVACkmN1ZP09wd1wHKN2DdPr004FytFYg9');
-    $customer = Stripe\Customer::create(array(
+
+    $stripeToken = $request->stripeToken;
+    $paymentMethod = Stripe\PaymentMethod::create([
+        "type" => "card",
+        "card" => [
+            "token" => $stripeToken,
+        ],
+    ]);
+
+    $customer = Stripe\Customer::create([
+        "address" => [
+            "line1" => "123 Main St",
+            "postal_code" => "94111",
+            "city" => "San Francisco",
+            "state" => "CA",
+            "country" => "US",
+        ],
+        "email" => "demo@gmail.com",
+        "name" => "Nitin Pujari",
+        "payment_method" => $paymentMethod->id,
+    ]);
+
+    $paymentIntent = Stripe\PaymentIntent::create([
+        "amount" => 100 * 100,
+        "currency" => "usd",
+        "customer" => $customer->id,
+        "description" => "Test payment from LaravelTus.com.",
+        "payment_method_types" => ["card"],
+        "payment_method" => $paymentMethod->id,
+        "shipping" => [
+            "name" => "Jenny Rosen",
             "address" => [
-                "line1" => "123 Main St",
-                "postal_code" => "94111",
+                "line1" => "510 Townsend St",
+                "postal_code" => "98140",
                 "city" => "San Francisco",
                 "state" => "CA",
                 "country" => "US",
             ],
-            "email" => "demo@gmail.com",
-            "name" => "Nitin Pujari",
-            "source" => $request->stripeToken
-        ));
+        ],
+    ]);
 
-    Stripe\Charge::create ([
-            "amount" => 100 * 100,
-            "currency" => "usd",
-            "customer" => $customer->id,
-            "description" => "Test payment from LaravelTus.com.",
-            "shipping" => [
-                "name" => "Jenny Rosen",
-                "address" => [
-                    "line1" => "510 Townsend St",
-                    "postal_code" => "98140",
-                    "city" => "San Francisco",
-                    "state" => "CA",
-                    "country" => "US",
-                ],
-            ]
-    ]); 
+    $paymentIntent->confirm();
+
     Session::flash('success', 'Payment successful!');
     return back();
 }
 
-    
 }
