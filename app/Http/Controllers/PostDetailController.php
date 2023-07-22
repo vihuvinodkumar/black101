2ff92ae24c5fffdb075028fbe8578f41d3e736f9
vinod kewat <vihuvinodkumar@gmail.com>
2023-07-22 14:17:48 +0530

    edit the post details

--- app/Http/Controllers/PostDetailController.php
+++ app/Http/Controllers/PostDetailController.php
@@ -15,36 +15,59 @@ class PostDetailController extends Controller
     {
         try {
             $auth = $this->decryptt($request->header("Authorization"));
+            $userCreatedAt = Carbon::parse($auth[0]['created_at']);
+            $currentYear = Carbon::now()->year;
+            $currentTime = Carbon::now();
+
             $data = DB::table('story')
-                ->where('created_at', '>=', Carbon::createFromDate(2023, 1, 1))
+                ->where(function ($query) use ($userCreatedAt, $currentYear, $currentTime) {
+                    if ($userCreatedAt->year === $currentYear) {
+                        if ($userCreatedAt->hour < 9) {
+                            // Show posts created at or after user's creation date
+                            $query->where('created_at', '>=', $userCreatedAt);
+                        } else {
+                            // Hide posts created before user's creation date and show posts created after user's creation date + 1 day
+                            $query->whereDate('created_at', '>', $userCreatedAt->addDay());
+                        }
+                    } else {
+                        // Show posts from the current year
+                        $query->whereYear('created_at', $currentYear);
+                    }
+                })
                 ->orderBy("created_at", "DESC")->get();
 
             $final_data = [];
 
-            for ($i = 0; $i < count($data); $i++) {
+            foreach ($data as $post) {
+                $postID = $post->id;
+                $postLikes = DB::table('likes')->where('post_id', $postID)->get();
+                $postRatings = DB::table('rating')->where('product_id', $postID)->get();
+                $myLikes = DB::table('likes')->where([['post_id', $postID], ['user_id', $auth[0]["id"]]])->get();
 
                 array_push(
                     $final_data,
                     [
-                        "post" => $data[$i],
-                        "rating" => DB::table('rating')->where('product_id', '=', $data[$i]->id)->get(),
-                        "likes" => DB::table('likes')->where('post_id', '=', $data[$i]->id)->get(),
-                        "my_likes" => DB::table('likes')->where([['post_id', $data[$i]->id], ['user_id', $auth[0]["id"]]])->get()
+                        "post" => $post,
+                        "rating" => $postRatings,
+                        "likes" => $postLikes,
+                        "my_likes" => $myLikes
                     ]
                 );
             }
+
             return response()->json([
                 "code" => 200,
-                "data" => $final_data
+                "data" => $final_data,
+                "created_at" => $userCreatedAt->hour
             ]);
         } catch (\Exception $e) {
             return response()->json([
                 "code" => 400,
                 "error" => $e->getMessage()
-            ])
-                ->setStatusCode(400);
+            ])->setStatusCode(400);
         }
     }
+    
     public function decryptt($token)
     {
         $exptoken = explode(".", $token);
